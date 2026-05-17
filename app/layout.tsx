765d1a1b7bc229861552b299a9939e4101fd5d7d
Kaung-Myat <kaungmyatthuu.dev@gmail.com>
2026-05-17 18:46:34 +0630

    Feat: Adding analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <Navbar />
         {children}
+         <Analytics />
       </body>
     </html>
   );
